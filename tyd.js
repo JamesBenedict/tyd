3c6eb155e0858bfc317f66fe17a9f5a61260f17d
James Benedict <jamesbenedict@Jamess-MacBook-Pro-2.local>
2018-11-15 20:34:40 -0500

    first draft done

--- tyd.js
+++ tyd.js
@@ -1,23 +1,25 @@
-export function tyd(data, yAxisKeyName) {
-    this.formattedData = []
-    this.columnNames = data['columns']
-    this.rowNames = [... new Set(data(map(item => item[yAxisKeyName])))];
+function tyd(data, yAxisKeyName) {
+    const formattedData = []
+    // const columnNames = data['columns']
+    // const rowNames = [... new Set(data.map(item => item[yAxisKeyName]))];
     let count = 0
     data.forEach((row) => {
+        
         let rowName = row[yAxisKeyName]
         for (let key in row) {
             let cellValue = row[key]
             // this passes header data,so  your column headers don't get treated as cells
-            if (cellValue != yName) {
+            if (cellValue != rowName) {
                 const output = {
                     x: key,
                     y: rowName,
-                    value: +cellValue
+                    value: cellValue
                 }
-                this.formattedData[count] = output
+                formattedData[count] = output
                 count += 1
             }
         }
     })
-    return this.formattedData
+    console.log('tyd', formattedData)
+    return formattedData
 }
