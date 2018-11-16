d2baab5da305ed76705e71905981a659a5c3b39e
James Benedict <jamesbenedict0@gmail.com>
2018-11-16 10:54:38 -0500

    clean up clean up everyone do their share

--- tyd.js
+++ tyd.js
@@ -1,14 +1,10 @@
 function tyd(data, yAxisKeyName) {
     const formattedData = []
-    // const columnNames = data['columns']
-    // const rowNames = [... new Set(data.map(item => item[yAxisKeyName]))];
     let count = 0
-    data.forEach((row) => {
-        
+    data.forEach((row) => { 
         let rowName = row[yAxisKeyName]
         for (let key in row) {
             let cellValue = row[key]
-            // this passes header data,so  your column headers don't get treated as cells
             if (cellValue != rowName) {
                 const output = {
                     x: key,
